bfa0a2e276601a6f38da0648bf6f49719717c13e
Rishabh Raghwendra <rishabh@bizionictech.com>
2025-02-17 12:59:26 +0530

    removed select class option

--- src/components/SelectDetails.jsx
+++ src/components/SelectDetails.jsx
@@ -231,9 +231,9 @@ const SelectDetails = () => {
               onChange={(e)=>handleTravelClass(e.target.value)}
               value={travelClass}
             >
-              <option value="ECONOMY" className="">
+              {/* <option value="SELECT_CLASS" className="">
                 Select Class
-              </option>
+              </option> */}
               <option value="ECONOMY">Economy</option>
               <option value="PREMIUM_ECONOMY">Premium Economy</option>
               <option value="BUSINESS">Business</option>
